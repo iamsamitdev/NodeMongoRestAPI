0308385b3481c55de99f2b6c234ef52c55da5be8
Samit Koyom <samitkoyom@gmail.com>
2021-09-24 02:25:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+// Setup express app
+const app = express()
+
+// Connect to mongodb
+mongoose.connect('mongodb://localhost:27017/nodejseventdb')
+mongoose.Promise = global.Promise
+
+app.use(express.json())
+
+// การสร้าง router ใน express
+// app.get('/api', (req, res) => res.send('It working!'))
+
+// โหลด router
+app.use('/api',require('./routes/api'))
+
+// error handling middleware
+app.use(function(err,req,res,next){
+    res.status(422).send({error: err.message})
+})
+
+app.listen(process.env.port || 4000, function(){
+    console.log('Ready to Go!')
+})
